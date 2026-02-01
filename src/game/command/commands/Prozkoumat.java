7ffe6b2d4169d52bcaa54011a20d487983b9c4cd
Matouš <hrus.matous@gmail.com>
2026-02-01 22:47:06 +0100

    Úprava výpisu u příkazu Prozkoumat.

--- src/game/command/commands/Prozkoumat.java
+++ src/game/command/commands/Prozkoumat.java
@@ -14,9 +14,23 @@ public class Prozkoumat extends Command {
         if (areAnyItemsInPlayerRoom(world)) {
             toReturn.append("Hmmm, tak v této místnosti vidím itemy: ");
             toReturn.append(itemsInPlayerRoomText(world));
+
+            if (areAnyNonMovableInteractableItemsInPlayerRoom(world)) {
+                toReturn.append("\n");
+                toReturn.append("A je tu dokonce použitelný: ");
+                toReturn.append(NonMovableInteractableItemsInPlayerRoomText(world));
+                toReturn.append("\n");
+            }
         }
         else {
             toReturn.append("Žádné předměty v této místnosti nevidím...");
+
+            if (areAnyNonMovableInteractableItemsInPlayerRoom(world)) {
+                toReturn.append("\n");
+                toReturn.append("Ale je zde alespoň použitelný: ");
+                toReturn.append(NonMovableInteractableItemsInPlayerRoomText(world));
+                toReturn.append("\n");
+            }
         }
         toReturn.append("\n");
 
@@ -39,18 +53,21 @@ public class Prozkoumat extends Command {
         return false;
     }
 
-    private boolean areAnyItemsInPlayerRoom(GameData world) {
+
+    private boolean areAnyNonMovableInteractableItemsInPlayerRoom(GameData world) {
         for (int i = 0; i < world.getItems().size(); i++) {
             if (UI.toLowercaseAscii(world.getPlayerRoom().getName()).equals(UI.toLowercaseAscii(world.getItems().get(i).getLocation()))) {
-                return true;
+                if (!world.getItems().get(i).isMovable() && world.getItems().get(i).isInteractable()) {
+                    //Bere v potaz, že právě nemovitý item je zároveň použitelný
+                    return true;
+                }
             }
         }
 
         return false;
     }
 
-    //TODO: ošetřit nepohyblivé itemy (Panel v komunikacích) a vytisknout je na nový řádek, něco jako sice tu nevidím žádné itemy, ale je tu nějaký panel... bla bla bla
-    private String itemsInPlayerRoomText(GameData world) {
+    private String NonMovableInteractableItemsInPlayerRoomText(GameData world) {
         StringBuilder toReturn = new StringBuilder();
 
         //Number of Items in the same room as the player
@@ -58,17 +75,21 @@ public class Prozkoumat extends Command {
 
         for (int i = 0; i < world.getItems().size(); i++) {
             if (UI.toLowercaseAscii(world.getPlayerRoom().getName()).equals(UI.toLowercaseAscii(world.getItems().get(i).getLocation()))) {
-                itemCount++;
+                if (!world.getItems().get(i).isMovable() && world.getItems().get(i).isInteractable()) {
+                    itemCount++;
+                }
             }
         }
 
         for (int i = 0; i < world.getItems().size(); i++) {
 
             if (UI.toLowercaseAscii(world.getPlayerRoom().getName()).equals(UI.toLowercaseAscii(world.getItems().get(i).getLocation()))) {
-                toReturn.append(world.getItems().get(i).getName());
+                if (!world.getItems().get(i).isMovable() && world.getItems().get(i).isInteractable()) {
+                    toReturn.append(world.getItems().get(i).getName());
 
-                if (i < itemCount - 1) {
-                    toReturn.append(", ");
+                    if (i < itemCount - 1) {
+                        toReturn.append(", ");
+                    }
                 }
             }
 
@@ -77,6 +98,52 @@ public class Prozkoumat extends Command {
         return toReturn.toString();
     }
 
+    //Které jsou movité
+    private boolean areAnyItemsInPlayerRoom(GameData world) {
+        for (int i = 0; i < world.getItems().size(); i++) {
+            if (UI.toLowercaseAscii(world.getPlayerRoom().getName()).equals(UI.toLowercaseAscii(world.getItems().get(i).getLocation()))) {
+                if (world.getItems().get(i).isMovable()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    //Jen pro movité
+    private String itemsInPlayerRoomText(GameData world) {
+        StringBuilder toReturn = new StringBuilder();
+
+        //Number of Items in the same room as the player
+        int itemCount = 0;
+
+        for (int i = 0; i < world.getItems().size(); i++) {
+            if (UI.toLowercaseAscii(world.getPlayerRoom().getName()).equals(UI.toLowercaseAscii(world.getItems().get(i).getLocation()))) {
+                if (world.getItems().get(i).isMovable()) {
+                    itemCount++;
+                }
+            }
+        }
+
+        for (int i = 0; i < world.getItems().size(); i++) {
+
+            if (UI.toLowercaseAscii(world.getPlayerRoom().getName()).equals(UI.toLowercaseAscii(world.getItems().get(i).getLocation()))) {
+                if (world.getItems().get(i).isMovable()) {
+                    toReturn.append(world.getItems().get(i).getName());
+
+                    if (i < itemCount - 1) {
+                        toReturn.append(", ");
+                    }
+                }
+            }
+
+        }
+
+        return toReturn.toString();
+    }
+    //TODO: jak je if(i < itemCount -1), opravit ze nahazim vsechny itemy ve stejny mistnosti jako hrac do arraylistu a v tom budu iterovat, takhle se nekdy napise bez těch čárek mezi elementy
+    //TODO: nebo vlastně to je možná kvůli tomu, že to tam je jeden item bez lokace, kterej právě drží hráč v inventáři
 
     private boolean areAnyCharactersInPlayerRoom(GameData world) {
         for (int i = 0; i < world.getCharacters().size(); i++) {
